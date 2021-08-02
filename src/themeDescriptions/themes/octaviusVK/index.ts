283b09e5a47626f02e33574af7b84a1137b73932
Sergey Peshkov <gpuzzletime@icloud.com>
2021-08-02 20:30:17 +0300

    fix-octavius-theme: add vk sans

--- src/themeDescriptions/themes/octaviusVK/index.ts
+++ src/themeDescriptions/themes/octaviusVK/index.ts
@@ -2,12 +2,230 @@ import {ParadigmThemeDescription} from '@/interfaces/namespaces/paradigm';
 import {lightColors as vkColors} from '@/themeDescriptions/base/vk';
 import {octaviusTheme} from '@/themeDescriptions/themes/octavius';
 
-export const octaviusVKTheme: ParadigmThemeDescription = {
+const fontFamilyAccent =
+	'VKSansDisplay, "TT Commons", -apple-system, system-ui, Helvetica Neue, Roboto, sans-serif';
+const fontFamilyBase =
+	'VKSansText, -apple-system, system-ui, "Helvetica Neue", Roboto, sans-serif';
+const fontWeightAccent2 = 500;
+const fontWeightAccent3 = 500;
+const fontWeightBase1 = 400;
+
+// TODO: Придумать более изящный способ наследования
+const octaviusVKThemeBase: ParadigmThemeDescription = {
 	...octaviusTheme,
 	themeName: 'octaviusVK',
+
+	// Цвета
 	colors: {
 		...octaviusTheme.colors,
 		...vkColors.colors,
 		colorBackgroundSecondary: 'rgba(0, 28, 61, 0.05)',
+	},
+
+	// Типографика
+	// Размеры такие же, как в теме octavius, а вот fontFamily другой
+	fontFamilyAccent,
+	fontFamilyBase,
+	fontTitle1: {
+		regular: {
+			fontSize: 24,
+			lineHeight: 28,
+			fontFamily: fontFamilyAccent,
+			fontWeight: fontWeightAccent3,
+		},
+	},
+	fontTitle2: {
+		regular: {
+			fontSize: 20,
+			lineHeight: 26,
+			fontFamily: fontFamilyAccent,
+			fontWeight: fontWeightAccent3,
+		},
+	},
+	fontTitle3: {
+		regular: {
+			fontSize: 17,
+			lineHeight: 24,
+			fontFamily: fontFamilyAccent,
+			fontWeight: fontWeightAccent3,
+		},
+	},
+	fontHeadline: {
+		regular: {
+			fontSize: 15,
+			lineHeight: 20,
+			fontFamily: fontFamilyAccent,
+			fontWeight: fontWeightAccent2,
+		},
+	},
+	// todo поменять местами fontSize, когда откажемся от auto
+	// todo fontSize в мобильном виде должен быть 16
+	fontText: {
+		regular: {
+			fontSize: 15,
+			lineHeight: 20,
+			fontFamily: fontFamilyBase,
+			fontWeight: fontWeightBase1,
+		},
+	},
+	fontSubhead: {
+		regular: {
+			fontSize: 14,
+			lineHeight: 18,
+			fontFamily: fontFamilyBase,
+			fontWeight: fontWeightBase1,
+		},
+	},
+	fontFootnote: {
+		regular: {
+			fontSize: 13,
+			lineHeight: 18,
+			fontFamily: fontFamilyBase,
+			fontWeight: fontWeightBase1,
+		},
+	},
+	fontFootnoteCaps: {
+		regular: {
+			fontSize: 13,
+			lineHeight: 18,
+			fontFamily: fontFamilyBase,
+			fontWeight: fontWeightBase1,
+			textTransform: 'uppercase',
+			letterSpacing: '0.3px',
+		},
+	},
+	fontCaption1: {
+		regular: {
+			fontSize: 12,
+			lineHeight: 16,
+			fontFamily: fontFamilyBase,
+			fontWeight: fontWeightBase1,
+		},
+	},
+	fontCaption1Caps: {
+		regular: {
+			fontSize: 12,
+			lineHeight: 16,
+			fontFamily: fontFamilyBase,
+			fontWeight: fontWeightBase1,
+			textTransform: 'uppercase',
+			letterSpacing: '0.26px',
+		},
+	},
+	fontCaption2: {
+		regular: {
+			fontSize: 11,
+			lineHeight: 14,
+			fontFamily: fontFamilyBase,
+			fontWeight: fontWeightBase1,
+		},
+	},
+	fontCaption2Caps: {
+		regular: {
+			fontSize: 11,
+			lineHeight: 14,
+			fontFamily: fontFamilyBase,
+			fontWeight: fontWeightBase1,
+			textTransform: 'uppercase',
+			letterSpacing: '0.22px',
+		},
+	},
+	fontCaption3: {
+		regular: {
+			fontSize: 9,
+			lineHeight: 12,
+			fontFamily: fontFamilyBase,
+			fontWeight: fontWeightBase1,
+		},
+	},
+	fontCaption3Caps: {
+		regular: {
+			fontSize: 9,
+			lineHeight: 12,
+			fontFamily: fontFamilyBase,
+			fontWeight: fontWeightBase1,
+			textTransform: 'uppercase',
+			letterSpacing: '0.16px',
+		},
+	},
+
+	// todo удалить и заменить на контекстные токены в проектах
+	fontH0: {
+		regular: {
+			fontSize: 28,
+			lineHeight: 32,
+			fontFamily: fontFamilyAccent,
+			fontWeight: fontWeightAccent3,
+		},
+		compact: {
+			fontSize: 44,
+			lineHeight: 52,
+		},
+	},
+	// todo удалить и заменить на контекстные токены в проектах
+	fontH1: {
+		regular: {
+			fontSize: 24,
+			lineHeight: 28,
+			fontFamily: fontFamilyAccent,
+			fontWeight: fontWeightAccent3,
+		},
+		compact: {
+			fontSize: 32,
+			lineHeight: 36,
+		},
+	},
+	// todo удалить и заменить на контекстные токены в проектах
+	fontH2: {
+		regular: {
+			fontSize: 20,
+			lineHeight: 26,
+			fontFamily: fontFamilyAccent,
+			fontWeight: fontWeightAccent3,
+		},
+		compact: {
+			fontSize: 28,
+			lineHeight: 32,
+		},
+	},
+};
+
+export const octaviusVKTheme: ParadigmThemeDescription = {
+	...octaviusVKThemeBase,
+	// todo удалить и заменить на контекстные токены в проектах
+	fontH3: {
+		regular: {
+			...octaviusVKThemeBase.fontTitle3.regular,
+		},
+		compact: {
+			...octaviusVKThemeBase.fontTitle1.regular,
+		},
+	},
+	// todo удалить и заменить на контекстные токены в проектах
+	fontH4: {
+		regular: {
+			...octaviusVKThemeBase.fontHeadline.regular,
+		},
+		compact: {
+			...octaviusVKThemeBase.fontTitle2.regular,
+		},
+	},
+	// todo удалить и заменить на контекстные токены в проектах
+	fontH5: {
+		regular: {
+			...octaviusVKThemeBase.fontHeadline.regular,
+		},
+		compact: {
+			...octaviusVKThemeBase.fontTitle3.regular,
+		},
+	},
+	// todo удалить и заменить на контекстные токены в проектах
+	fontH6: {
+		regular: {
+			...octaviusVKThemeBase.fontHeadline.regular,
+		},
+		compact: {
+			...octaviusVKThemeBase.fontHeadline.regular,
+		},
 	},
 };
