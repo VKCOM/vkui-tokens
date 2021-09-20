03e8047fca0bb6c0d54c35bf70e480887c8b5acf
Konstantin Zubanov <k.zubanov@corp.mail.ru>
2021-09-20 09:47:05 +0300

    поменял токен заглушки картинки на новую логику с дефолтным непрозрачным цветом

--- src/interfaces/general/colors/index.ts
+++ src/interfaces/general/colors/index.ts
@@ -62,7 +62,7 @@ export interface ColorsDescriptionStruct {
 	colorTrackBuffer: ColorDescription;
 	colorBackgroundModal: ColorDescription;
 	colorImagePlaceholder: ColorDescription;
-	colorImagePlaceholderOpaque: ColorDescription;
+	colorImagePlaceholderAlpha: ColorDescription;
 	colorSearchFieldBackground: ColorDescription;
 	colorFieldBorder: ColorDescription;
 	colorSeparatorPrimaryAlpha: ColorDescription;
