abc73de48dd17f554f7d71012f3719e3a756185c
g.andrievskii <Grigorii>
2022-05-25 16:37:01 +0300

    vkui search theme button updated

--- src/themeDescriptions/themes/search/index.ts
+++ src/themeDescriptions/themes/search/index.ts
@@ -5,7 +5,23 @@ export const searchTheme: ThemeSearchDescription = {
 	...lightTheme,
 	themeName: 'search',
 	themeNameBase: 'search',
+	colors: {
+		...lightTheme.colors,
+		colorTextPrimary: '#2C2D2E',
+	},
 	sizeBorderRadius: {
 		regular: 8,
+	},
+	sizeButtonLargeHeight: {
+		regular: 44,
+		compact: 36,
+	},
+	sizeButtonMediumHeight: {
+		regular: 36,
+		compact: 32,
+	},
+	sizeButtonSmallHeight: {
+		regular: 30,
+		compact: 28,
 	},
 };
