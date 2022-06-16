c6c9d082d0f10636d3e1469bd5b8f6b5efb95be9
Sergei Peshkov <s.peshkov@corp.mail.ru>
2022-06-16 15:14:12 +0300

    4.13.0: add gradient unit test

---
+++ src/build/helpers/getGradientPointsFromColor.test.ts
@@ -0,0 +1,14 @@
+import {getGradientPointsFromColor} from '@/build/helpers/getGradientPointsFromColor';
+
+describe('getGradientPointsFromColor', () => {
+	it('should generate gradient from color', () => {
+		expect(getGradientPointsFromColor('rgb(0, 100, 200)')).toEqual(
+			'rgba(0, 100, 200, 0) 0%, ' +
+				'rgba(0, 100, 200, 0.036) 13%, ' +
+				'rgba(0, 100, 200, 0.15) 27%, ' +
+				'rgba(0, 100, 200, 0.79) 68%, ' +
+				'rgba(0, 100, 200, 0.95) 84%, ' +
+				'rgba(0, 100, 200, 1) 100%',
+		);
+	});
+});
