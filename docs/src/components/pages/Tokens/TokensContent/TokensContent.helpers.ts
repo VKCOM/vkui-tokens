ce580745aac487f80de0acb82a21cf654576f317
Sergei Peshkov <s.peshkov@corp.mail.ru>
2022-12-01 20:26:52 +0300

    docs-comments: improve search filtering

--- docs/src/components/pages/Tokens/TokensContent/TokensContent.helpers.ts
+++ docs/src/components/pages/Tokens/TokensContent/TokensContent.helpers.ts
@@ -1,16 +1,42 @@
-import {TokenItem} from '@/shared/types';
+import {TokenItem, TokenItemValue} from '@/shared/types';
 
-export const filterByTags = (
+export function filterByTags(
 	selectedTags: Array<string>,
 	token: TokenItem,
-): boolean => {
+): boolean {
+	// Исключаем legacy-токены по умолчанию, если не запросили явно
+	if (!selectedTags.includes('legacy') && token.tags.includes('legacy')) {
+		return false;
+	}
+
 	if (selectedTags.length === 0) {
 		return true;
 	}
-	return selectedTags.some((sTag) => token.tags.includes(sTag));
-};
 
-export const filterByDesc = (searchValue: string, token: TokenItem): boolean =>
-	searchValue === ''
-		? true
-		: token.desc.toLowerCase().includes(searchValue.toLowerCase());
+	return selectedTags.some((sTag) => token.tags.includes(sTag));
+}
+
+function containsInDesc(haystack: string, needle: string): boolean {
+	return haystack.toLowerCase().includes(needle);
+}
+
+function containsInValue(haystack: TokenItemValue, needle: string): boolean {
+	const haystackNorm = JSON.stringify(haystack)
+		.toLowerCase()
+		.replace(/\s/g, '');
+
+	return haystackNorm.includes(needle.replace(/\s/g, ''));
+}
+
+export function filterByDesc(searchValue: string, token: TokenItem): boolean {
+	if (searchValue === '') {
+		return true;
+	}
+
+	const needle = searchValue.toLowerCase();
+
+	return (
+		containsInDesc(token.desc, needle) ||
+		containsInValue(token.value, needle)
+	);
+}
