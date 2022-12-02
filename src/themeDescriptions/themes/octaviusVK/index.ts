c6389bc0683faa5e7ea12e43c911581fe52ea49d
Danil Ryzhov <daniil.ryzhov@vk.team>
2022-12-02 19:51:11 +0300

    fix tokens values

--- src/themeDescriptions/themes/octaviusVK/index.ts
+++ src/themeDescriptions/themes/octaviusVK/index.ts
@@ -506,8 +506,8 @@ export const octaviusVKDarkTheme: ThemeOctaviusDescription = {
 			hover: 'none',
 			active: 'none',
 		},
-		octaviusColorPromoBackground: '#232324',
-		octaviusColorPlaceholderBackground: '#3E3E3F',
+		octaviusColorPromoBackground: '#292929',
+		octaviusColorPlaceholderBackground: '#161616',
 	},
 
 	octaviusElevationCard: 'none',
