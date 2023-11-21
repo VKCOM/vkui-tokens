e5f3a60598268d29dd98a7407fb90a136634b688
p.kochura <p.kochura@vk.team>
2023-11-21 15:49:06 +0300

    Add dobro theme

--- src/interfaces/themes/dobro/index.ts
+++ src/interfaces/themes/dobro/index.ts
@@ -1,25 +1,47 @@
-import type { Property } from 'csstype';
-import type { ThemeDescription } from "@/interfaces/general";
+import type { Theme, ThemeCssVars, ThemeDescription } from '@/interfaces/general';
 import type { Adaptive } from '@/interfaces/general/tools';
 import type { Fonts, TypographyBaseProps } from '@/interfaces/general/typography';
 
-export interface ThemeDescriptionDobro extends ThemeDescription {
-  zIndexMailMenu: number;
-  zIndexPopover: number;
-  zIndexPopup: number;
-  zIndexMenu: number;
-  widthToTouchS: string;
-  widthTouchM: string;
-  widthToTouchM: string;
-  widthTouchL: string;
-  widthToTouch: string;
-  widthFromTablet: string;
-  widthTablet: string;
-  widthToTablet: string;
-  widthFromDesktopS: string;
-  widthDesktopS: string;
-  widthToDesktopS: string;
-  widthFromDesktopM: string;
+export interface ThemeDobro
+	extends Theme,
+		ThemeDobroMedia,
+		ThemeDobroCustomTokens,
+		ThemeDobroFonts {}
+
+export interface ThemeDobroDescription
+	extends ThemeDescription,
+		ThemeDobroMedia,
+		ThemeDobroCustomTokens,
+		ThemeDobroFonts {}
+
+export interface ThemeDobroCssVars extends ThemeCssVars<ThemeDobro> {}
+
+interface ThemeDobroCustomTokens {
+	zIndexMailMenu: number;
+	zIndexPopover: number;
+	zIndexPopup: number;
+	zIndexMenu: number;
+}
+
+export interface ThemeDobroFonts {
+	fontFamilyDisplay: string;
+	fontFamilyDisplayMedium: string;
+	fontFamilyDisplayRegular: string;
+}
+
+export interface ThemeDobroMedia {
+	widthToTouchS: string;
+	widthTouchM: string;
+	widthToTouchM: string;
+	widthTouchL: string;
+	widthToTouch: string;
+	widthFromTablet: string;
+	widthTablet: string;
+	widthToTablet: string;
+	widthFromDesktopS: string;
+	widthDesktopS: string;
+	widthToDesktopS: string;
+	widthFromDesktopM: string;
 }
 
 export type BaseFonts = Omit<
@@ -28,8 +50,4 @@ export type BaseFonts = Omit<
 	},
 	'fontHeadline'
 > &
-	TypographyBaseProps & {
-    fontFamilyDisplay: Property.FontFamily;
-    fontFamilyDisplayMedium: Property.FontFamily;
-    fontFamilyDisplayRegular: Property.FontFamily;
-  };
+	TypographyBaseProps;
--- src/themeDescriptions/themes/dobro/index.ts
+++ src/themeDescriptions/themes/dobro/index.ts
@@ -1,99 +1,53 @@
-import { replacePropDeep } from '@/build/helpers/replacePropDeep';
-import figma from '@/themeDescriptions/base/figma/vk.json';
-import { colorFromFigma, darkElevation, darkGradient, fonts as fontsVKBase, lightTheme } from '@/themeDescriptions/base/vk';
 import type { ColorsDescription } from '@/interfaces/general';
-import type { BaseFonts, ThemeDescriptionDobro } from '@/interfaces/themes/dobro';
+import type { ThemeDobroDescription } from '@/interfaces/themes/dobro';
+import {
+	colorFromFigma,
+	darkElevation,
+	darkGradient,
+	fonts,
+	lightTheme,
+} from '@/themeDescriptions/base/vk';
 
-const fontFamilyDisplay = '"VK Sans Display", sans-serif, Arial';
-const fontFamilyDisplayMedium = '"VK Sans Display Medium", sans-serif, Arial';
-const fontFamilyDisplayRegular = '"VK Sans Display Regular", sans-serif, Arial';
+const fontFamilyDisplay = 'VK Sans Display, sans-serif, Arial';
+const fontFamilyDisplayMedium = 'VK Sans Display Medium, sans-serif, Arial';
+const fontFamilyDisplayRegular = 'VK Sans Display Regular, sans-serif, Arial';
 
-const lightColors: ColorsDescription = replacePropDeep(colorFromFigma('light'), {
-  colorBackgroundSecondary: {
-    light: {
-      normal: figma.light.background.background_secondary,
-      hover: figma.light.states.background_secondary.hover,
-      active: 'rgba(0, 0, 0, 0.12)',
-    },
-  },
-});
+const lightColors: ColorsDescription = colorFromFigma('light');
 
 const darkColors: ColorsDescription = colorFromFigma('dark');
 
-const fontWeightAccent1 = 600;
+export const dobroTheme: ThemeDobroDescription = {
+	...lightTheme,
+	...lightColors,
+	...fonts,
 
-const fonts: BaseFonts = {
-  ...fontsVKBase,
-  fontFamilyDisplay,
-  fontFamilyDisplayMedium,
-  fontFamilyDisplayRegular,
-	fontDisplayTitle1: {
-		regular: {
-			fontSize: 23,
-			lineHeight: 28,
-			fontFamily: fontFamilyDisplay,
-			fontWeight: fontWeightAccent1,
-		},
-	},
-	fontDisplayTitle2: {
-		regular: {
-			fontSize: 21,
-			lineHeight: 26,
-			fontFamily: fontFamilyDisplay,
-			fontWeight: fontWeightAccent1,
-		},
-		compact: {
-			fontSize: 22,
-			lineHeight: 24,
-			fontFamily: fontFamilyDisplay,
-			fontWeight: fontWeightAccent1,
-		},
-	},
-	fontDisplayTitle3: {
-		regular: {
-			fontSize: 19,
-			lineHeight: 24,
-			fontFamily: fontFamilyDisplay,
-			fontWeight: fontWeightAccent1,
-		},
-	},
-	fontDisplayTitle4: {
-		regular: {
-			fontSize: 17,
-			lineHeight: 24,
-			fontFamily: fontFamilyDisplay,
-			fontWeight: fontWeightAccent1,
-		},
-	},
-};
-
-export const dobroTheme: ThemeDescriptionDobro = {
-  ...lightTheme,
-  ...lightColors,
-  ...fonts,
 	themeName: 'dobro',
 	themeNameBase: 'dobro',
 
-  zIndexMailMenu: 96,
-  zIndexPopover: 96,
-  zIndexPopup: 97,
-  zIndexMenu: 98,
+	fontFamilyDisplay,
+	fontFamilyDisplayMedium,
+	fontFamilyDisplayRegular,
 
-  widthToTouchS: '(max-width: 499px)',
-  widthTouchM: '(min-width: 500px) and (max-width: 637px)',
-  widthToTouchM: '(max-width: 638px)',
-  widthTouchL: '(min-width: 639px) and (max-width: 767px)',
-  widthToTouch: '(max-width: 767px)',
-  widthFromTablet: '(min-width: 768px)',
-  widthTablet: '(min-width: 768px) and (max-width: 1003px)',
-  widthToTablet: '(max-width: 1003px)',
-  widthFromDesktopS: '(min-width: 1004px)',
-  widthDesktopS: '(min-width: 1004px) and (max-width: 1244px)',
-  widthToDesktopS: '(max-width: 1244px)',
-  widthFromDesktopM: '(min-width: 1245px)',
+	zIndexMailMenu: 96,
+	zIndexPopover: 96,
+	zIndexPopup: 97,
+	zIndexMenu: 98,
+
+	widthToTouchS: '(max-width: 499px)',
+	widthTouchM: '(min-width: 500px) and (max-width: 637px)',
+	widthToTouchM: '(max-width: 638px)',
+	widthTouchL: '(min-width: 639px) and (max-width: 767px)',
+	widthToTouch: '(max-width: 767px)',
+	widthFromTablet: '(min-width: 768px)',
+	widthTablet: '(min-width: 768px) and (max-width: 1003px)',
+	widthToTablet: '(max-width: 1003px)',
+	widthFromDesktopS: '(min-width: 1004px)',
+	widthDesktopS: '(min-width: 1004px) and (max-width: 1244px)',
+	widthToDesktopS: '(max-width: 1244px)',
+	widthFromDesktopM: '(min-width: 1245px)',
 };
 
-export const dobroDarkTheme: ThemeDescriptionDobro = {
+export const dobroDarkTheme: ThemeDobroDescription = {
 	...dobroTheme,
 	...darkColors,
 	...darkGradient,
