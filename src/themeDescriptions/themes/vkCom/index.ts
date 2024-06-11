594dfd4fc627209d887be6472ab0e606e53d1a1e
nick <wrl.snk@gmail.com>
2024-06-11 17:30:12 +0300

    Updated fonts in vkCom theme

--- src/themeDescriptions/themes/vkCom/index.ts
+++ src/themeDescriptions/themes/vkCom/index.ts
@@ -15,8 +15,8 @@ import {
 
 import { resolveColor } from './appearance';
 
-const fontFamilyAccent = '-apple-system, system-ui, "Helvetica Neue", Roboto, sans-serif';
-const fontFamilyBase = '-apple-system, system-ui, "Helvetica Neue", Roboto, sans-serif';
+const fontFamilyAccent = '"VK Sans Display", "VK Sans Display Faux", -apple-system,BlinkMacSystemFont,"Roboto","Helvetica Neue",Geneva,"Noto Sans Armenian","Noto Sans Bengali","Noto Sans Cherokee","Noto Sans Devanagari","Noto Sans Ethiopic","Noto Sans Georgian","Noto Sans Hebrew","Noto Sans Kannada","Noto Sans Khmer","Noto Sans Lao","Noto Sans Osmanya","Noto Sans Tamil","Noto Sans Telugu","Noto Sans Thai",arial,Tahoma,verdana,sans-serif';
+const fontFamilyBase = '-apple-system,BlinkMacSystemFont,"Roboto","Helvetica Neue",Geneva,"Noto Sans Armenian","Noto Sans Bengali","Noto Sans Cherokee","Noto Sans Devanagari","Noto Sans Ethiopic","Noto Sans Georgian","Noto Sans Hebrew","Noto Sans Kannada","Noto Sans Khmer","Noto Sans Lao","Noto Sans Osmanya","Noto Sans Tamil","Noto Sans Telugu","Noto Sans Thai",arial,Tahoma,verdana,sans-serif';
 const fontWeightAccent2 = 500;
 const fontWeightBase3 = 400;
 
