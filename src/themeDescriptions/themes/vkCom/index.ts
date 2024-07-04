5a0f2c467c2f220debbbf20d2062b5dbb929ceb3
Daniil Suvorov <severecloud@gmail.com>
2024-07-04 12:11:24 +0300

    fix: fontFamilyFallbacks & set fontFamilyBase

--- src/themeDescriptions/themes/vkCom/index.ts
+++ src/themeDescriptions/themes/vkCom/index.ts
@@ -20,6 +20,7 @@ const fontFamilyAccent =
 	'"VK Sans Display", "VK Sans Display Faux", -apple-system, BlinkMacSystemFont, "Roboto", "Helvetica Neue", Geneva, "Noto Sans Armenian", "Noto Sans Bengali", "Noto Sans Cherokee", "Noto Sans Devanagari", "Noto Sans Ethiopic", "Noto Sans Georgian", "Noto Sans Hebrew", "Noto Sans Kannada", "Noto Sans Khmer", "Noto Sans Lao", "Noto Sans Osmanya", "Noto Sans Tamil", "Noto Sans Telugu", "Noto Sans Thai", arial, Tahoma, verdana, sans-serif';
 const fontFamilyBase =
 	'-apple-system, BlinkMacSystemFont, "Roboto", "Helvetica Neue", Geneva, "Noto Sans Armenian", "Noto Sans Bengali", "Noto Sans Cherokee", "Noto Sans Devanagari", "Noto Sans Ethiopic", "Noto Sans Georgian", "Noto Sans Hebrew", "Noto Sans Kannada", "Noto Sans Khmer", "Noto Sans Lao", "Noto Sans Osmanya", "Noto Sans Tamil", "Noto Sans Telugu", "Noto Sans Thai", arial, Tahoma, verdana, sans-serif';
+const fontFamilyFallbacks = fontFamilyBase;
 const fontWeightAccent2 = 500;
 const fontWeightBase3 = 400;
 
@@ -156,6 +157,7 @@ const vkComLightColor: ColorsDescription = {
 };
 
 const vkComFontsPartial: DeepPartial<typeof fonts> = {
+	fontFamilyFallbacks,
 	fontFamilyAccent,
 	fontFamilyBase,
 
@@ -184,22 +186,22 @@ const vkComFontsPartial: DeepPartial<typeof fonts> = {
 	},
 	fontTitle1: {
 		regular: {
-			fontFamily: fontFamilyAccent,
+			fontFamily: fontFamilyBase,
 		},
 	},
 	fontTitle2: {
 		regular: {
-			fontFamily: fontFamilyAccent,
+			fontFamily: fontFamilyBase,
 		},
 	},
 	fontTitle3: {
 		regular: {
-			fontFamily: fontFamilyAccent,
+			fontFamily: fontFamilyBase,
 		},
 	},
 	fontHeadline2: {
 		regular: {
-			fontFamily: fontFamilyAccent,
+			fontFamily: fontFamilyBase,
 		},
 	},
 	fontParagraph: {
