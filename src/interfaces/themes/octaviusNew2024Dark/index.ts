94310b5be6f3406f22d426e6f5cb6f0b3268560d
Sarafa2n <slaft3@yandex.ru>
2024-07-08 16:07:21 +0300

    fix: build

---
+++ src/interfaces/themes/octaviusNew2024Dark/index.ts
@@ -0,0 +1,9 @@
+import {
+	ThemeOctavius,
+	ThemeOctaviusCssVars,
+	ThemeOctaviusDescription,
+} from '@/interfaces/themes/octavius';
+
+export interface ThemeOctaviusNew2024Dark extends ThemeOctavius {}
+export interface ThemeOctaviusNew2024DarkDescription extends ThemeOctaviusDescription {}
+export interface ThemeOctaviusNew2024DarkCssVars extends ThemeOctaviusCssVars {}
