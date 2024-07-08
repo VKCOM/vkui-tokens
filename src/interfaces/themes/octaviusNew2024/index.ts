089dcdcf5a52cc10547b3cec72a77dc52b876750
Sarafa2n <slaft3@yandex.ru>
2024-07-08 16:03:41 +0300

    fix: add tests and docs

---
+++ src/interfaces/themes/octaviusNew2024/index.ts
@@ -0,0 +1,9 @@
+import {
+	ThemeOctavius,
+	ThemeOctaviusCssVars,
+	ThemeOctaviusDescription,
+} from '@/interfaces/themes/octavius';
+
+export interface ThemeOctaviusNew2024 extends ThemeOctavius {}
+export interface ThemeOctaviusNew2024Description extends ThemeOctaviusDescription {}
+export interface ThemeOctaviusNew2024CssVars extends ThemeOctaviusCssVars {}
