a515e2d71ef9372d84b61f4cfca1451b899e62ab
Vadim Khizhnyakov <v.khizhnyakov@vk.team>
2024-07-16 17:29:19 +0300

    feat: calendar theme mailsans -> vksans

--- src/themeDescriptions/themes/calendar/index.ts
+++ src/themeDescriptions/themes/calendar/index.ts
@@ -9,7 +9,7 @@ import { fontSizes, helpers } from '../../common';
 
 const { x2, x4, x5 } = helpers;
 
-export const fontFamily: Property.FontFamily = 'MailSans, Helvetica, Arial, sans-serif';
+export const fontFamily: Property.FontFamily = 'VKSansDisplay, Helvetica, Arial, sans-serif';
 
 const fontSize2 = {
 	...fontSizes.fontSize2,
