c32b91b58b17ac067cb6fa60a394c0023e53424c
Vadim Khizhnyakov <v.khizhnyakov@vk.team>
2024-07-18 17:43:17 +0300

    feat: paradigm base accent font family from css var

--- src/themeDescriptions/base/paradigm.ts
+++ src/themeDescriptions/base/paradigm.ts
@@ -8,8 +8,9 @@ import { Elevation } from '@/interfaces/general/elevation';
 import { ParadigmThemeDescription } from '@/interfaces/namespaces/paradigm';
 import { helpers } from '@/themeDescriptions/common';
 
+const fontFamilyUser = 'var(--vkui-font-family-accent, MailSans)';
 const fontFamilyFallbacks = 'Helvetica, Arial, sans-serif';
-const fontFamilyAccent = `MailSans, ${fontFamilyFallbacks}`;
+const fontFamilyAccent = `${fontFamilyUser}, ${fontFamilyFallbacks}`;
 const fontFamilyBase = fontFamilyFallbacks;
 const fontWeightAccent1 = 500;
 const fontWeightAccent2 = 500;
