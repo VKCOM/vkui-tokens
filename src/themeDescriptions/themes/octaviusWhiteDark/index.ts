e1c89441d1dbcc05153b7ef3e68722220f7033dc
Дмитрий Пешков <dmitrii.peshkov@vk.team>
2024-07-25 16:16:32 +0300

    FMAIL-24962: dark

--- src/themeDescriptions/themes/octaviusWhiteDark/index.ts
+++
@@ -1,9 +0,0 @@
-import {
-	ThemeOctavius,
-	ThemeOctaviusCssVars,
-	ThemeOctaviusDescription,
-} from '@/interfaces/themes/octavius';
-
-export type ThemeOctaviusWhiteDark = ThemeOctavius;
-export type ThemeOctaviusWhiteDarkDescription = ThemeOctaviusDescription;
-export type ThemeOctaviusWhiteDarkCssVars = ThemeOctaviusCssVars;
