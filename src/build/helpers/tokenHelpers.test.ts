6eb7fc0eb28ef9fa338b6a57c072c22058dcb6ca
s.peshkov <s.peshkov@corp.mail.ru>
2024-09-19 19:50:27 +0300

    feat/tokenized-gradients: wip new gradient helper function

--- src/build/helpers/tokenHelpers.test.ts
+++ src/build/helpers/tokenHelpers.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test } from '@jest/globals';
 
-import { alias, staticRef } from '@/build/helpers/tokenHelpers';
+import { alias, gradient, namedAlias, staticRef } from '@/build/helpers/tokenHelpers';
 
 describe('tokenHelpers', () => {
 	describe('alias', () => {
@@ -8,6 +8,17 @@ describe('tokenHelpers', () => {
 			expect(alias('sizeArrow')({ sizeArrow: { regular: 10 } })).toEqual({
 				regular: 10,
 			});
+		});
+	});
+
+	describe('namedAlias', () => {
+		test('maps token to another token, preserving name', () => {
+			expect(namedAlias('sizeArrow')({ sizeArrow: { regular: 10 } })).toEqual([
+				'sizeArrow',
+				{
+					regular: 10,
+				},
+			]);
 		});
 	});
 
@@ -19,6 +30,23 @@ describe('tokenHelpers', () => {
 		test('fails for dynamic value', () => {
 			expect(() => staticRef(alias('sizeArrow'))).toThrowError(
 				'Cannot use callable token value in static ref',
+			);
+		});
+	});
+
+	describe('gradient', () => {
+		test('calculates gradient string', () => {
+			const gradientToken = gradient([namedAlias('colorIconPrimary'), 'transparent']);
+			const gradientValue = gradientToken({ colors: { colorIconPrimary: 'blue' } as any });
+
+			expect(gradientValue).toEqual(
+				'var(--vkui--color_icon_primary, rgba(0, 0, 255, 1)) 0%, rgba(0, 0, 0, 0, 0) 100%',
+			);
+		});
+
+		test('fails if bad number of arguments is given', () => {
+			expect(() => gradient([namedAlias('colorIconPrimary')])).toThrowError(
+				'Gradient stops length (1) is not equal to the number of opacity points given (2)',
 			);
 		});
 	});
--- src/build/helpers/tokenHelpers.ts
+++ src/build/helpers/tokenHelpers.ts
@@ -1,8 +1,27 @@
-import { ThemeGeneral } from '@/interfaces/general';
+import type { Property } from 'csstype';
+
+import { ThemeDescription } from '@/interfaces/general';
 import { Token } from '@/interfaces/general/tools/tokenValue';
 
-export function alias<T extends ThemeGeneral>(token: string): (theme: Partial<T>) => Token<any, T> {
-	return (theme) => theme[token];
+import { getGradientPointsFromColor, makeGradientPointRaw } from './getGradientPointsFromColor';
+
+export type TokenFunction<T extends ThemeDescription> = (theme: Partial<T>) => Token<any, T>;
+export type NamedTokenFunction<T extends ThemeDescription> = (
+	theme: Partial<T>,
+) => [string | undefined, Token<any, T>];
+
+export type OpacityPoint = [number, number];
+
+function readThemeToken(theme: any, token: string): Token<any, any> {
+	return theme[token] ?? (theme['colors'] ?? {})[token];
+}
+
+export function alias<T extends ThemeDescription>(token: string): TokenFunction<T> {
+	return (theme) => readThemeToken(theme, token);
+}
+
+export function namedAlias<T extends ThemeDescription>(token: string): NamedTokenFunction<T> {
+	return (theme) => [token, readThemeToken(theme, token)];
 }
 
 export function staticRef<T>(value: Token<T, any>): T {
@@ -11,4 +30,33 @@ export function staticRef<T>(value: Token<T, any>): T {
 	}
 
 	return value;
+}
+
+export function gradient<T extends ThemeDescription>(
+	stops: (Property.Color | NamedTokenFunction<T>)[],
+	opacityPoints: OpacityPoint[] = [
+		[1, 0],
+		[1, 100],
+	],
+): TokenFunction<T> {
+	if (stops.length !== opacityPoints.length) {
+		throw new Error(
+			`Gradient stops length (${
+				stops.length
+			}) is not equal to the number of opacity points given (${opacityPoints.length})`,
+		);
+	}
+
+	return (theme) => {
+		return opacityPoints
+			.map(([pointOpacity, pointCoordinate], index) => {
+				const stop = stops[index];
+				const [stopKey, stopValue] = typeof stop === 'function' ? stop(theme) : [undefined, stop];
+
+				const pointRaw = makeGradientPointRaw(stopValue, stopKey);
+
+				return getGradientPointsFromColor(pointRaw, 1, [[pointOpacity, pointCoordinate]]);
+			})
+			.join(', ');
+	};
 }
