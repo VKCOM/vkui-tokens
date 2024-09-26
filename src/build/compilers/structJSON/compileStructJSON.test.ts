99c470a86407cf53114d0e7f8ab015bef02dabf1
s.peshkov <s.peshkov@corp.mail.ru>
2024-09-26 16:39:24 +0300

    feat/tokenized-gradients: add more tests

--- src/build/compilers/structJSON/compileStructJSON.test.ts
+++ src/build/compilers/structJSON/compileStructJSON.test.ts
@@ -10,6 +10,14 @@ describe('compileJSON', () => {
 			randomToken: 123,
 			gradientTest:
 				'var(--vkui--color_background_content, rgba(255, 255, 255, 1)) 0%, rgba(0, 0, 0, 0) 100%',
+			gradientOneColor: [
+				'rgba(0, 0, 255, 0) 0%',
+				'rgba(0, 0, 255, 0.05) 15%',
+				'rgba(0, 0, 255, 0.2) 30%',
+				'rgba(0, 0, 255, 0.8) 70%',
+				'rgba(0, 0, 255, 0.95) 85%',
+				'rgba(0, 0, 255, 1) 100%',
+			].join(', '),
 		};
 
 		expect(compileStructJSON(testData)).toBe(
@@ -31,6 +39,26 @@ describe('compileJSON', () => {
 			},
 			{
 				"color": "rgba(0, 0, 0, 0)"
+			}
+		],
+		"gradientOneColor": [
+			{
+				"color": "rgba(0, 0, 255, 0)"
+			},
+			{
+				"color": "rgba(0, 0, 255, 0.05)"
+			},
+			{
+				"color": "rgba(0, 0, 255, 0.2)"
+			},
+			{
+				"color": "rgba(0, 0, 255, 0.8)"
+			},
+			{
+				"color": "rgba(0, 0, 255, 0.95)"
+			},
+			{
+				"color": "rgba(0, 0, 255, 1)"
 			}
 		]
 	}
