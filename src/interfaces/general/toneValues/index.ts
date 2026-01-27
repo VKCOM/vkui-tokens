4e6929b6b3ce28e7ddb671c8996b7645b5932bdc
nick <wrl.snk@gmail.com>
2026-01-27 19:32:27 +0300

    feat(vkBase): Updated opacity for disabled elements

--- src/interfaces/general/toneValues/index.ts
+++ src/interfaces/general/toneValues/index.ts
@@ -8,6 +8,9 @@ export interface ToneValues {
 	/**
 	 * @desc Прозрачность неинтерактивных элементов, подходящая под параметры доступности
 	 * @tags opacity
+	 * @deprecated
+	 * Неинтерактивные элементы не обязаны соответствовать требованиям доступности по контрастности
+	 * https://www.w3.org/WAI/WCAG21/Understanding/non-text-contrast.html#inactive-controls
 	 */
 	opacityDisableAccessibility: number;
 
