8ad49ec7f6d228e4de7f42ef1f0dd8d0c69fcd54
nick <wrl.snk@gmail.com>
2026-01-28 14:50:20 +0300

    fixed typo

--- src/interfaces/general/toneValues/index.ts
+++ src/interfaces/general/toneValues/index.ts
@@ -1,15 +1,15 @@
 export interface ToneValues {
 	/**
-	 * @desc Прозрачность неинтерактивных элементов
+	 * @desc Прозрачность неактивных элементов
 	 * @tags opacity
 	 */
 	opacityDisable: number;
 
 	/**
-	 * @desc Прозрачность неинтерактивных элементов, подходящая под параметры доступности
+	 * @desc Прозрачность неактивных элементов, подходящая под параметры доступности
 	 * @tags opacity
 	 * @deprecated
-	 * Неинтерактивные элементы не обязаны соответствовать требованиям доступности по контрастности
+	 * Неактивные элементы не обязаны соответствовать требованиям доступности по контрастности
 	 * https://www.w3.org/WAI/WCAG21/Understanding/non-text-contrast.html#inactive-controls
 	 */
 	opacityDisableAccessibility: number;
