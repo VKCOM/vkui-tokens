9aea965792779b076f10d4a2b20d41b3388857fb
nick <wrl.snk@gmail.com>
2026-05-21 18:00:51 +0300

    help

--- src/interfaces/themes/legoIOS/index.ts
+++ src/interfaces/themes/legoIOS/index.ts
@@ -3,13 +3,32 @@ import type {
 	ColorsDescriptionStruct,
 	ColorWithStates,
 } from 'interfaces/general/colors/index.ts';
+import type { GradientPoints } from 'interfaces/general/gradients/index.ts';
 import type { ThemeCssVars } from 'interfaces/general/index.ts';
 
 import type { ThemeVkBase, ThemeVkBaseDescription } from '../vkBase/index.ts';
 
+/**
+ * Дополнительные цвета для темы
+ */
 export interface LocalVkontakteIOSColorsDescriptionStruct {
-	// Дополнительные цвета для темы
 	colorBackgroundInverse: ColorDescription;
+
+	componentsGradientButtonOverlayPrimaryStroke: GradientPoints;
+	componentsColorButtonOverlayPrimaryStroke1: ColorDescription;
+	componentsColorButtonOverlayPrimaryStroke2: ColorDescription;
+	componentsColorButtonOverlayPrimaryBackground: ColorDescription;
+
+	componentsGradientButtonNeutralPrimaryStroke: GradientPoints;
+	componentsColorButtonNeutralPrimaryStroke1: ColorDescription;
+	componentsColorButtonNeutralPrimaryStroke2: ColorDescription;
+	componentsColorButtonNeutralPrimaryBackground: ColorDescription;
+
+	componentsGradientContextMenuStroke: GradientPoints;
+	componentsColorContextMenuStroke1: ColorDescription;
+	componentsColorContextMenuStroke2: ColorDescription;
+	componentsColorContextMenuBackground: ColorDescription;
+	componentsColorContextMenuColorDodger: ColorDescription;
 }
 
 export type LegoIOSLocalColors = {
