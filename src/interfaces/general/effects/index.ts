f06b433b1a56b4cbb72f9102c0895e275e75db3f
nick <wrl.snk@gmail.com>
2026-06-15 23:37:49 +0300

    feat(vkBase, lego): Update colors, add bold fonts and blurs

--- src/interfaces/general/effects/index.ts
+++ src/interfaces/general/effects/index.ts
@@ -1,7 +1,25 @@
 export interface Effects {
 	/**
+	 * @desc Слабое размытие
+	 * @tags blur
+	 */
+	blurS: number;
+
+	/**
 	 * @desc Среднее размытие
 	 * @tags blur
 	 */
 	blurM: number;
+
+	/**
+	 * @desc Сильное размытие
+	 * @tags blur
+	 */
+	blurL: number;
+
+	/**
+	 * @desc Очень сильное размытие
+	 * @tags blur
+	 */
+	blurXL: number;
 }
